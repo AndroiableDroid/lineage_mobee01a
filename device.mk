ada6f15773b207cf5a737baf63baaa157fea7cbb
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-02-04 23:08:21 +0530

    LS-5015: Add VoLTE

--- device.mk
+++ device.mk
@@ -215,7 +215,9 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
+    init.qti.ims.sh \
     fstab.qcom \
+    init.target.rc \
     init.qcom.bt.sh \
     init.qcom.power.rc \
     init.qcom.rc \
@@ -258,3 +260,26 @@ PRODUCT_COPY_FILES += \
 # WCNSS Service
 PRODUCT_PACKAGES += \
     wcnss_service
+
+# IMS properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.lte.volte_call_capable=true \
+    persist.ims.enableADBLogs=1 \
+    persist.ims.enableDebugLogs=1 \
+    persist.radio.calls.on.ims=true \
+    persist.radio.csvt.enabled=false \
+    persist.radio.jbims=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.volte_enabled_by_hw=1 \
+    DEVICE_PROVISIONED=1 \
+    ro.telephony.default_network=9,9 \
+    ril.subscription.types=NV,RUIM
+
+
+# Widevine DRM symbol, boringssl-compat subset
+PRODUCT_PACKAGES += \
+    libshim_boringssl
